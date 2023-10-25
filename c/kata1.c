76ecf90efc557654ccff94316f12195c4ea16071
toufy <ar.toufic@protonmail.com>
2023-10-26 00:55:28 +0300

    learning c

---
+++ c/kata1.c
@@ -0,0 +1,11 @@
+// Beginner Series #1 School Paperwork
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+
+int paperwork(int n, int m)
+{
+    if (n > 0 && m > 0)
+    {
+        return m * n;
+    }
+    return 0;
+}
